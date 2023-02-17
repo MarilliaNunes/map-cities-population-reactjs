071191c2cb3258a7d9ab345862d7a56a35e66ab2
Marillia Vasconcelos <marilliavasconceloscn@gmail.com>
2023-02-16 22:56:43 -0300

    refactor: change html and main files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from '../presentation/pages/index'
+import { App } from './presentation/pages/index'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
